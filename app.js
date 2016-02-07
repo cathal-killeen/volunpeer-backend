151cb0b0fc2b8d7b1b0977278199fbacb10727a4
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-07 13:58:09 +0000

    added todo array and api

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ var express = require('express');
 var app = express();
 //process.env.PORT provided by heroku
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: "Go for lunch",
+    completed: false
+},
+{
+    id: 2,
+    description: "Go to market",
+    completed: false
+}];
 
-app.get('/', function(req,res){
+app.get('/', function(req, res){
     res.send('Todo API root');
 });
+
+//get request /todos
+app.get('/todos', function(req, res){
+    res.json(todos);
+});
+
+//todos/:id
+//app.get('')
 
 app.listen(PORT, function(){
     console.log("Listening on port " + PORT + "...");
