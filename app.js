c58181f663b21e9d3a33cdd4400ac04c86030ca8
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-07 16:57:42 +0000

    added POST for todos

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 //process.env.PORT provided by heroku
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: "Go for lunch",
-    completed: false
-},
-{
-    id: 2,
-    description: "Go to market",
-    completed: false
-},
-{
-    id: 3,
-    description: "Buy eggs",
-    completed: true
-}];
+var todos = [];
+
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.send('Todo API root');
@@ -37,6 +28,20 @@ app.get('/todos/:id', function(req, res){
         }
     })
     res.status(404).send();
+});
+
+// post /todos
+app.post('/todos', function(req, res){
+    var body = req.body;
+    todos.push({
+        "id": todoNextId,
+        "description": body.description,
+        "completed": body.completed
+    });
+    todoNextId++;
+    console.log('description: ' + body.description);
+
+    res.json(body);
 });
 
 app.listen(PORT, function(){
