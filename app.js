07e2a4b09b491089adeef6a8daa2a0f436c66114
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-07 13:40:02 +0000

    Added basic server

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+//process.env.PORT provided by heroku
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+    res.send('Todo API root');
+});
+
+app.listen(PORT, function(){
+    console.log("Listening on port " + PORT + "...");
+});
