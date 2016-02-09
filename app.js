b6f4f13c8dc5f51a3b97710e4cc78d76a65ed2b7
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-09 00:08:05 +0000

    fixed bug in PUT method

--- app.js
+++ app.js
@@ -105,16 +105,16 @@ app.put('/todos/:id', function(req,res){
 
     db.todo.findById(req.params.id).then(function(todo){
         if(todo){
-            return todo.update(attributes);
+            todo.update(attributes).then(function(todo){
+                res.json(todo.toJSON());
+            }, function(e){
+                res.status(400).json(e).send();
+            });
         }else{
             res.status(404).send();
         }
     }, function(){
         res.status(500).send();
-    }).then(function(todo){
-        res.json(todo.toJSON());
-    }, function(e){
-        res.status(400).json(e).send();
     });
 });
 
